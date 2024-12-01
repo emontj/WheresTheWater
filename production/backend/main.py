2e903a844d078441d7d4cdd3bfe50eb858aabee4
emontj <emontj@users.noreply.github.com>
2024-12-01 10:41:09 -0700

    main SQL connection

--- production/backend/main.py
+++ production/backend/main.py
@@ -3,8 +3,10 @@ Copyright @emontj 2024
 """
 
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.sqlite3'
 
 @app.route("/")
 def main():
@@ -20,5 +22,12 @@ def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
 
+db = SQLAlchemy(app)
+class Users(db.Model):
+    id = db.Column("User_ID", db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
