46001cd62516b4183bb43c609785dc34f6d94111
emontj <emontj@users.noreply.github.com>
2024-12-06 17:55:44 -0700

    improvements to aesthetics and search bar basics

---
+++ production/frontend/static/searchBar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchForm = document.getElementById('searchForm');
+    searchForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const query = document.getElementById('searchInput').value;
+
+        // TODO Implement search logic here
+    });
+});
